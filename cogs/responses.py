f17273135ab111006bf8bdb36b4f6b75f3f96d99
hillari <hillari@gmail.com>
2019-11-22 22:31:49 -0900

    Added more commands to the cogs

--- cogs/responses.py
+++ cogs/responses.py
@@ -1,7 +1,7 @@
 import asyncio
 import random
 import os
-
+import discord
 import aiohttp
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -20,12 +20,40 @@ class Responses(commands.Cog):
         await ctx.send("The current prefix is: " + prefix)
 
     @commands.command()
+    async def joined(self, ctx, member: discord.Member = None):
+        """When member joined the server. e.g. joined <optional: @user>"""
+        if member is None:
+            await ctx.send('{0.name} joined in {0.joined_at}'.format(ctx.message.author))
+        else:
+            await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+    @commands.command()
     async def hello(self, ctx):
         """Greets you"""
         await ctx.send('Hello, {0.author.mention} , I see you!'.format(ctx))
 
+    @commands.command()
+    async def membercount(self, ctx):
+        """Displays Total Number of Users in the Server"""
+        id = self.bot.get_guild(ctx.message.guild.id)
+        await ctx.message.delete()
+        await ctx.send(f"""{id.member_count}""" + " members in the server")
 
+    @commands.command()
+    # @commands.has_role('Grader People')
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def say(self, ctx, *, arg):
+        """Says what you want it to e.g. say <text>"""
+        await ctx.message.delete()
+        await ctx.send(arg)
 
+    @commands.command()
+    #@commands.has_role("Grader People")
+    async def setstatus(self, ctx, *, status):
+        """Sets Bot Status e.g. setstatus <status>"""
+        game = discord.Game(status)
+        await ctx.message.delete()
+        await self.bot.change_presence(status=discord.Status.online, activity=game)
 
 
 def setup(bot):
--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+import mysql.connector
 
 load_dotenv()
 owner = os.getenv('owner')
@@ -8,3 +9,52 @@ owner = os.getenv('owner')
 
 def is_owner():
     return commands.check(lambda ctx: ctx.message.author.id == owner)
+
+def db_connect():
+    db = mysql.connector.connect(
+        host='localhost',
+        database='cs_discord_bot',
+        user='hillari',
+        # charset='utf8mb4',
+        password='password',
+    )
+    # print("Connection ID:", db.connection_id)
+    # print(db)
+    return db
+
+
+def init_all_users(ctx):
+    for guild in ctx.guilds:
+        for member in guild.members:
+            print(member.name)
+            print(member.id)
+            add_user_to_db(member,guild)
+
+
+def add_user_to_db(member, guild):
+    db = db_connect()
+    mycursor = db.cursor()
+    # with db.cursor() as cursor:
+    # id = (str(guild.id) + str(member.id))
+    sql = "INSERT INTO members(discord_id, username, role, " \
+          "experience, level, reputation, warnings) " \
+          "VALUES (%s,%s,%s,%s,%s,%s,%s);"
+    mycursor.execute(sql, (member.id, str(member.name), "member", 0, 0, 0, 0))
+
+
+
+
+# mysql> show columns in members;
+# +------------+--------------+------+-----+---------+----------------+
+# | Field      | Type         | Null | Key | Default | Extra          |
+# +------------+--------------+------+-----+---------+----------------+
+# | id         | int(11)      | NO   | PRI | NULL    | auto_increment |
+# | discord_id | int(11)      | NO   |     | NULL    |                |
+# | username   | varchar(255) | YES  |     | NULL    |                |
+# | role       | varchar(255) | YES  |     | NULL    |                |
+# | experience | int(11)      | YES  |     | NULL    |                |
+# | level      | int(11)      | YES  |     | NULL    |                |
+# | reputation | int(11)      | YES  |     | NULL    |                |
+# | warnings   | varchar(255) | YES  |     | NULL    |                |
+# +------------+--------------+------+-----+---------+----------------+
+
